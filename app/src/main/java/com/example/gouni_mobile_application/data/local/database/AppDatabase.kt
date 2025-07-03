29b107a680dfda148c40ef7bee7c5c2fec68018f
AaronAlejandrou <165164022+AaronAlejandrou@users.noreply.github.com>
2025-07-03 02:38:07 -0500

    feat: add clean code on unsed carpets

--- app/src/main/java/com/example/gouni_mobile_application/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/gouni_mobile_application/data/local/database/AppDatabase.kt
@@ -2,8 +2,6 @@ package com.example.gouni_mobile_application.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.gouni_mobile_application.data.local.dao.CarDao
 import com.example.gouni_mobile_application.data.local.dao.ReservationDao
 import com.example.gouni_mobile_application.data.local.dao.RouteDao
@@ -23,44 +21,4 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun routeDao(): RouteDao
     abstract fun reservationDao(): ReservationDao
     abstract fun carDao(): CarDao
-}
-
-val MIGRATION_1_2 = object : Migration(1, 2) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE users ADD COLUMN userCode TEXT NOT NULL DEFAULT ''")
-    }
-}
-
-val MIGRATION_2_3 = object : Migration(2, 3) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-
-        database.execSQL("""
-            CREATE TABLE cars (
-                id TEXT PRIMARY KEY NOT NULL,
-                userId TEXT NOT NULL,
-                make TEXT NOT NULL,
-                model TEXT NOT NULL,
-                licensePlate TEXT NOT NULL,
-                color TEXT NOT NULL,
-                year INTEGER NOT NULL,
-                insuranceInfo TEXT NOT NULL,
-                registrationNumber TEXT NOT NULL
-            )
-        """)
-
-        database.execSQL("ALTER TABLE routes ADD COLUMN carId TEXT NOT NULL DEFAULT ''")
-    }
-}
-
-val MIGRATION_3_4 = object : Migration(3, 4) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-
-        database.execSQL("ALTER TABLE cars ADD COLUMN insuranceBrand TEXT NOT NULL DEFAULT ''")
-    }
-}
-
-val MIGRATION_4_5 = object : Migration(4, 5) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE reservations ADD COLUMN universityName TEXT NOT NULL DEFAULT ''")
-    }
 }
